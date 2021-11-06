bfb4f068cbf265f5321e23db1319c10956b8fbf4
Charly Mietton <charlymietton@MacBook-Pro-de-Charly.home>
2021-11-06 18:35:39 +0100

    trying to fix CI/CD

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -10,17 +10,6 @@ class NavBar extends React.Component {
             <img src={book} alt="logo" width="112" height="28" />
           </a>
 
-          <a
-            role="button"
-            className="navbar-burger"
-            aria-label="menu"
-            aria-expanded="false"
-            data-target="navbarBasicExample"
-          >
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-            <span aria-hidden="true"></span>
-          </a>
         </div>
         <div id="navbarBasicExample" className="navbar-menu">
           <div className="navbar-start">
