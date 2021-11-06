b7aff12c0c71cc39fc974de65dfac580e3270073
marly chietton <marly@mac-mini-de-marly.home>
2021-11-06 17:51:22 +0100

    add container to center it all

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -10,25 +10,27 @@ class HomePage extends React.Component {
     return (
       <section className="hero is-fullheight-with-navbar">
         <div className="hero-body">
-          <div className="columns is-vcentered">
-            <div className="column">
-              <div className="container has-text-left">
-                <h1 className="title">Home Section de Lues</h1>
-                <h2 className="subtitle">
-                  C'est ici qu'il faut insérer la première description de{" "}
-                  <strong>texte</strong>, notamment y mettre le contenu
-                  souhaité.
-                </h2>
-                <button
-                  className="button is-link"
-                  onClick={() => this.props.showPresentationPageOnClick()}
-                >
-                  Learn More
-                </button>
+          <div className="container">
+            <div className="columns is-vcentered">
+              <div className="column is-half">
+                <div className="container has-text-left">
+                  <h1 className="title">Home Section de Lues</h1>
+                  <h2 className="subtitle">
+                    C'est ici qu'il faut insérer la première description de{" "}
+                    <strong>texte</strong>, notamment y mettre le contenu
+                    souhaité.
+                  </h2>
+                  <button
+                    className="button is-link"
+                    onClick={() => this.props.showPresentationPageOnClick()}
+                  >
+                    Learn More
+                  </button>
+                </div>
               </div>
-            </div>
-            <div className="column is-v">
-              <img src={image1} width="512" height="512" />
+              <div className="column is-half">
+                <img src={image1} width="512" height="512" />
+              </div>
             </div>
           </div>
         </div>
